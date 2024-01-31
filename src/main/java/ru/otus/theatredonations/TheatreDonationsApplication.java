5efd5b1a3015e0ceff3f744f5c9bed6861709686
Yuriy Khodin <khodiniua@alfastrah.ru>
2024-01-31 23:32:48 +0300

    initial commit, oauth2 client for donationalerts in progress

---
+++ src/main/java/ru/otus/theatredonations/TheatreDonationsApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.theatredonations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheatreDonationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheatreDonationsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/otus/theatredonations/config/SecurityConfig.java
@@ -0,0 +1,8 @@
+package ru.otus.theatredonations.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfig {
+
+}
